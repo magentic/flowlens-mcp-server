9e00453ac4291fb247469b54149e95bc8726f17c
Selmy <selmy@magentic.ai>
2025-10-05 17:17:45 +0200

    * improve docs

--- local_mcp_server/flowlens_mcp/tools.py
+++ local_mcp_server/flowlens_mcp/tools.py
@@ -9,7 +9,8 @@ from ..service import flow_lens, timeline
 @server_instance.flowlens_mcp.tool
 async def list_flows(ctx: Context) -> dto.FlowList:
     """
-    List all flows for the authenticated user.
+    List all flows for the authenticated user. the returned flows is a summary of the flow.
+    For full details of a flow use get_flow tool. 
     Returns:
         dto.FlowList: List of Flow dto objects.
     """
@@ -19,7 +20,9 @@ async def list_flows(ctx: Context) -> dto.FlowList:
 @server_instance.flowlens_mcp.tool
 async def get_flow(flow_id: int, ctx: Context) -> dto.FlowlensFlow:
     """
-    Get a specific flow by its ID.
+    Get a specific full flow by its ID. It contains all flow data including a summary of timeline events 
+    e.g. number of events, status codes distribution, events types distribution, etc.
+    It is a very important entry point to start investigating a flow.
     Args:
         flow_id (int): The ID of the flow to retrieve.
     Returns:
@@ -115,7 +118,9 @@ async def create_shareable_link(flow_id: int, ctx: Context) -> dto.FlowShareLink
 @server_instance.flowlens_mcp.tool
 async def get_flow_timeline_events_within_range(flow_id: int, start_index: int, end_index: int, ctx: Context) -> str:
     """
-    Get timeline events for a specific flow within a range of indices.
+    Get timeline events for a specific flow within a range of indices. this returns a summary of the events in one line each.
+    each line starts with the event index, event_type, action_type, relative_timestamp, and the rest is data depending on the event type.
+    If you need full details of an event use get_full_flow_timeline_event_by_index tool using the flow_id and event_index.
     Args:
         flow_id (int): The ID of the flow to retrieve events for.
         start_index (int): The starting index of the events to retrieve.
@@ -129,7 +134,9 @@ async def get_flow_timeline_events_within_range(flow_id: int, start_index: int, 
 @server_instance.flowlens_mcp.tool
 async def get_flow_timeline_events_within_duration(flow_id: int, start_relative_time_ms: int, end_relative_time_ms: int, ctx: Context) -> str:
     """
-    Get timeline events for a specific flow within a duration range.
+    Get timeline events for a specific flow within a duration range. this returns a summary of the events in one line each.
+    each line starts with the event index, event_type, action_type, relative_timestamp, and the rest is data depending on the event type.
+    If you need full details of an event use get_full_flow_timeline_event_by_index tool using the flow_id and event_index.
     Args:
         flow_id (int): The ID of the flow to retrieve events for.
         start_relative_time_ms (int): The starting time in milliseconds of the events to retrieve. it is relative to the start of the recording.
@@ -143,7 +150,9 @@ async def get_flow_timeline_events_within_duration(flow_id: int, start_relative_
 @server_instance.flowlens_mcp.tool
 async def get_full_flow_timeline_event_by_index(flow_id: int, event_index: int, ctx: Context) -> dto.TimelineEventType:
     """
-    Get a full timeline event for a specific flow by its index.
+    Get a full timeline event for a specific flow by its index. headers and body fields are potentially trucated to avoid very large responses (max 50 chars).
+    If you need the full headers and body use get_network_request_full_headers_by_index, get_network_response_full_headers_by_index,
+    get_network_request_full_body_by_index, get_network_response_full_body_by_index tools using the flow_id and event_index.
     Args:
         flow_id (int): The ID of the flow to retrieve the event for.
         event_index (int): The index of the event to retrieve.
@@ -159,7 +168,10 @@ async def get_full_flow_timeline_event_by_index(flow_id: int, event_index: int, 
 @server_instance.flowlens_mcp.tool
 async def get_network_request_full_headers_by_index(flow_id: int, event_index: int, ctx: Context) -> dict:
     """
-    Get network request full headers for a specific flow by event index.
+    Get network request full headers for a specific flow by event index. This is important to understand the context of the request.
+    so you can see all headers including authentication headers, cookies, user-agent, etc. 
+    It helps you understand what the client is dealing with the server. and include tracing headers for debugging.
+    which is very important for debugging API calls. and can be used to investigate using observability tools like datadog, jaeger, etc.
     Args:
         flow_id (int): The ID of the flow to retrieve the event for.
         event_index (int): The index of the event to retrieve headers for.
@@ -172,7 +184,8 @@ async def get_network_request_full_headers_by_index(flow_id: int, event_index: i
 @server_instance.flowlens_mcp.tool
 async def get_network_response_full_headers_by_index(flow_id: int, event_index: int, ctx: Context) -> dict:
     """
-    Get network response full headers for a specific flow by event index.
+    Get network response full headers for a specific flow by event index. This is important to understand the context of the response.
+    so you can see all headers including content-type, content-encoding, set-cookie, etc. It helps you understand how the server responded to the request.
     Args:
         flow_id (int): The ID of the flow to retrieve the event for.
         event_index (int): The index of the event to retrieve headers for.
@@ -185,7 +198,9 @@ async def get_network_response_full_headers_by_index(flow_id: int, event_index: 
 @server_instance.flowlens_mcp.tool
 async def get_network_request_full_body_by_index(flow_id: int, event_index: int, ctx: Context) -> str:
     """
-    Get network request full body for a specific flow by event index.
+    Get network request full body for a specific flow by event index. This is important to understand the context of the request.
+    so you can see the full payload sent to the server. This is especially important for POST, PUT, PATCH requests.
+    it helps you understand what data is being sent to the server.
     Args:
         flow_id (int): The ID of the flow to retrieve the event for.
         event_index (int): The index of the event to retrieve the request body for.
@@ -198,7 +213,8 @@ async def get_network_request_full_body_by_index(flow_id: int, event_index: int,
 @server_instance.flowlens_mcp.tool
 async def get_network_response_full_body_by_index(flow_id: int, event_index: int, ctx: Context) -> str:
     """
-    Get network response full body for a specific flow by event index.
+    Get network response full body for a specific flow by event index. This is important to understand the context of the response.
+    so you can see the full payload sent by the server. which is very important for debugging API calls. and understanding the data sent by the server.
     Args:
         flow_id (int): The ID of the flow to retrieve the event for.
         event_index (int): The index of the event to retrieve the response body for.
