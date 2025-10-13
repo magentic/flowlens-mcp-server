283be849f808b49daec082b478a992a8ef05c998
Selmy <selmy@magentic.ai>
2025-10-13 18:29:44 +0200

    * doc string

--- flowlens_mcp_server/flowlens_mcp/tools.py
+++ flowlens_mcp_server/flowlens_mcp/tools.py
@@ -184,7 +184,7 @@ async def search_flow_events_with_regex(flow_id: int, pattern: str, event_type: 
     - NetworkRequestWithResponseEvent (network request and response)
     [index:int] network_request_with_response debugger_request_with_response [relative_timestamp:int]ms [POST|PUT|PATCH|GET|DELETE] [url:string] {[trace_id=opentelemtry_trace_id:string]:Optional} {[datadog_trace_id=datadog_trace_id:string]:Optional} status_code=[status_code:int] duration=[duration:int]ms
     - NetworkRequestPending (request sent but no response received yet)
-    [index:int] network_request_pending debugger_request_pending [relative_timestamp:int]ms [POST|PUT|PATCH|GET|DELETE] [url:string] {[trace_id=opentelemtry_trace_id:string]:Optional}
+    [index:int] network_request_pending debugger_request_pending [relative_timestamp:int]ms [POST|PUT|PATCH|GET|DELETE] [url:string] {[trace_id=opentelemtry_trace_id:string]:Optional} {[datadog_trace_id=datadog_trace_id:string]:Optional} 
     - DomActionEvent (click, keydown_session, scroll, etc.)
     [index:int] dom_action [click|keydown_session|scroll|etc.] [relative_timestamp:int]ms [element_text:string or element_src:string]
     - NavigationEvent (page navigation)
