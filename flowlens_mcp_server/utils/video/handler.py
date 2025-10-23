e8b34e54157fa817cee41735ff5c2a374da45578
Selmy <selmy@magentic.ai>
2025-10-23 19:21:09 +0200

    * remove redundant funciton

--- flowlens_mcp_server/utils/video/handler.py
+++ flowlens_mcp_server/utils/video/handler.py
@@ -31,22 +31,16 @@ class VideoHandler:
         await self._download_video()
 
     async def save_screenshot(self, video_sec: int) -> str:
-        frame_info = await self._extract_frame_async(video_sec)
+        frame_info = await asyncio.to_thread(self._extract_frame_buffer, video_sec)
         os.makedirs(self._video_dir_path, exist_ok=True)
-        # print(f"Time frames: {_FRAME_TIMESTAMP_CACHE}\n")
         output_path = os.path.join(self._video_dir_path, f"screenshot_sec{video_sec}.jpg")
 
         async with aiofiles.open(output_path, "wb") as f:
             await f.write(bytearray(frame_info.buffer))
         return os.path.abspath(output_path)
 
-    async def _extract_frame_async(self, video_sec):
-        return await asyncio.to_thread(self._extract_frame_buffer, video_sec)
-
     def _extract_frame_buffer(self, video_sec) -> _FrameInfo:
         video_path = os.path.join(self._video_dir_path, self._video_name)
-
-        
         cap = cv2.VideoCapture(video_path)
         frame = None
         ts = -1
