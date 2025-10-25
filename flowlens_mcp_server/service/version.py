f5f3ee209efe95bc8b18c33931306c8b3dc17061
Selmy <selmy@magentic.ai>
2025-10-25 09:25:59 +0200

    * rename

--- flowlens_mcp_server/service/version.py
+++ flowlens_mcp_server/service/version.py
@@ -5,28 +5,27 @@ from ..utils.settings import settings
 
 
 class VersionService:
-    _latest_version: dto.McpVersionResponse = None
+    _latest_version_check: dto.McpVersionResponse = None
     def __init__(self):
         base_url = f"{settings.flowlens_url}/mcp"
         self._client = HttpClient(settings.flowlens_api_token, base_url)
 
     @property
-    def latest_version(self) -> dto.McpVersionResponse:
-        if VersionService._latest_version is None:
+    def latest_version_check(self) -> dto.McpVersionResponse:
+        if VersionService._latest_version_check is None:
             self.check_version()
-        return VersionService._latest_version
+        return VersionService._latest_version_check
     
     def check_version(self) -> dto.McpVersionResponse:
         response = self._check_version()
-        VersionService._latest_version = response
+        VersionService._latest_version_check = response
         return response
     
     def assert_supported_version(self):
-        if self.latest_version.is_supported:
+        if self.latest_version_check.is_supported:
             return
         raise Exception(
-            f"Current MCP version {settings.flowlens_mcp_version} is not supported.\n"
-            f"Please, upgrade flowlens-mcp-server to latest version using command \"pipx upgrade flowlens-mcp-server\""
+            self.latest_version_check.message
         )
 
     def _check_version(self) -> dto.McpVersionResponse:
